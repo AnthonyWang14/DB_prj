98c50514cb27a299434853afb5f18d31c243bd07
winsen <nanhaiwinsen@163.com>
2015-12-27 03:45:48 -0800

    new

---
+++ SystemManagement/def.h
@@ -0,0 +1,19 @@
+#ifndef DEF
+#define DEF
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+/*
+ * 
+ */
+
+#define MAX_PATH 100
+#define DB_ROOT "/home/winsen/Desktop/db/database"
+#define CURRENTDB "currentDb.txt"
+#define ALLDB "allDb.txt"
+#define TABLELIST "tablelist.txt"
+
+#endif
